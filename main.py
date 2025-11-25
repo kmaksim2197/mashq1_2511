b7f3c4a3720a71358f10ec9e7468bde8ddeea904
kmaksim2197 <kmaksim2197@gmail.com>
2025-11-25 14:34:51 +0500

    Update and rename main.by to main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# 1-misol
 docs = input("Hujjat topshirilganmi? (ha/yo'q): ").strip().lower()
 interview = input("Suhbatdan o'tdingizmi? (ha/yo'q): ").strip().lower()
 test = input("Testdan o'tdingizmi? (ha/yo'q): ").strip().lower()
@@ -12,3 +13,27 @@ elif docs == "ha" and interview == "ha" and test == "yo'q":
     print("Test natijalari yetarli emas.")
 else:
     print("Jarayon davom etmoqda.")
+
+# 2-misol
+s = input()
+words = s.split()
+code = ""
+for w in words:
+    code += w[0]
+print(code)
+
+# 3-misol
+words = eval(input())
+
+max1 = ""
+max2 = ""
+
+for w in words:
+    if len(w) > len(max1):
+        max2 = max1
+        max1 = w
+    elif len(w) > len(max2) and w != max1:
+        max2 = w
+
+print("1-chi eng uzun so'z:", max1)
+print("2-chi eng uzun so'z:", max2)
